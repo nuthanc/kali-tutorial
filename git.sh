08ef7e13f8b2616a8437ffd987dfcefe9b04ebd5
nuthanc <nuthanc@juniper.net>
2020-04-19 17:36:00 +0530

    Add git

---
+++ git.sh
@@ -0,0 +1,32 @@
+TMPDIR=/var/folders/cr/9qm1hpm56bv2j1v33z9cwv1r0000gn/T/
+__CF_USER_TEXT_ENCODING=0x1F5:0:2
+SHELL=/bin/zsh
+HOME=/Users/nuthanc
+SSH_AUTH_SOCK=/private/tmp/com.apple.launchd.lcUF3Qupz5/Listeners
+Apple_PubSub_Socket_Render=/private/tmp/com.apple.launchd.Z9RgpwsXcR/Render
+LOGNAME=nuthanc
+PATH=/Users/nuthanc/bin:/usr/local/bin:/usr/local/anaconda3/bin:/Users/nuthanc/.npm/bin:./node_modules/.bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/nuthanc/bin:/usr/local/anaconda3/bin:/Users/nuthanc/.npm/bin:./node_modules/.bin:/Users/nuthanc/golang/bin:/usr/local/opt/go/libexec/bin:/Users/nuthanc/golang/bin:/usr/local/opt/go/libexec/bin
+XPC_SERVICE_NAME=0
+SECURITYSESSIONID=186a9
+USER=nuthanc
+XPC_FLAGS=0x0
+GOPATH=/Users/nuthanc/golang
+GOROOT=/usr/local/opt/go/libexec
+LESS=-R
+LSCOLORS=Gxfxcxdxbxegedabagacad
+NODE_PATH=/usr/local/lib/node_modules
+OLDPWD=/Users/nuthanc/personal_projects/kali
+PAGER=less
+PWD=/Users/nuthanc/personal_projects/kali
+SHLVL=1
+ZSH=/Users/nuthanc/.oh-my-zsh
+TERM_PROGRAM=vscode
+TERM_PROGRAM_VERSION=1.44.0
+LANG=en_GB.UTF-8
+COLORTERM=truecolor
+TERM=xterm-256color
+_=/usr/bin/printenv
+
+git add --all
+git commit -m "Add git"
+git push origin master
---
+++ git_acp.py
@@ -0,0 +1,22 @@
+import os
+from git import Repo
+
+PATH_OF_GIT_REPO = os.getcwd() + "/.git"
+commit_message = 'Remove enumerating SMB hahaha'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(commit_message)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except Exception as e:
+        print(e)
+
+git_push()
+
+# git_add_all = os.system("git add --all")
+# git_commit = os.system(f"git commit -m 'Update {commit_message}'")
+# git_push = os.system("git push origin master")
+# print(f"`git push origin master` ran with {git_push} exit status")
