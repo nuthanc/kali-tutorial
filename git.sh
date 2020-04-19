1cbbd37372d97ec2c6a2294b9d09aaa36a036bf7
nuthanc <nuthanc@juniper.net>
2020-04-19 17:38:00 +0530

    Update git

--- git.sh
+++ git.sh
@@ -1,32 +1,32 @@
-TMPDIR=/var/folders/cr/9qm1hpm56bv2j1v33z9cwv1r0000gn/T/
-__CF_USER_TEXT_ENCODING=0x1F5:0:2
-SHELL=/bin/zsh
-HOME=/Users/nuthanc
-SSH_AUTH_SOCK=/private/tmp/com.apple.launchd.lcUF3Qupz5/Listeners
-Apple_PubSub_Socket_Render=/private/tmp/com.apple.launchd.Z9RgpwsXcR/Render
-LOGNAME=nuthanc
-PATH=/Users/nuthanc/bin:/usr/local/bin:/usr/local/anaconda3/bin:/Users/nuthanc/.npm/bin:./node_modules/.bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/nuthanc/bin:/usr/local/anaconda3/bin:/Users/nuthanc/.npm/bin:./node_modules/.bin:/Users/nuthanc/golang/bin:/usr/local/opt/go/libexec/bin:/Users/nuthanc/golang/bin:/usr/local/opt/go/libexec/bin
-XPC_SERVICE_NAME=0
-SECURITYSESSIONID=186a9
-USER=nuthanc
-XPC_FLAGS=0x0
-GOPATH=/Users/nuthanc/golang
-GOROOT=/usr/local/opt/go/libexec
-LESS=-R
-LSCOLORS=Gxfxcxdxbxegedabagacad
-NODE_PATH=/usr/local/lib/node_modules
-OLDPWD=/Users/nuthanc/personal_projects/kali
-PAGER=less
-PWD=/Users/nuthanc/personal_projects/kali
-SHLVL=1
-ZSH=/Users/nuthanc/.oh-my-zsh
-TERM_PROGRAM=vscode
-TERM_PROGRAM_VERSION=1.44.0
-LANG=en_GB.UTF-8
-COLORTERM=truecolor
-TERM=xterm-256color
-_=/usr/bin/printenv
+export TMPDIR=/var/folders/cr/9qm1hpm56bv2j1v33z9cwv1r0000gn/T/
+export __CF_USER_TEXT_ENCODING=0x1F5:0:2
+export SHELL=/bin/zsh
+export HOME=/Users/nuthanc
+export SSH_AUTH_SOCK=/private/tmp/com.apple.launchd.lcUF3Qupz5/Listeners
+export Apple_PubSub_Socket_Render=/private/tmp/com.apple.launchd.Z9RgpwsXcR/Render
+export LOGNAME=nuthanc
+export PATH=/Users/nuthanc/bin:/usr/local/bin:/usr/local/anaconda3/bin:/Users/nuthanc/.npm/bin:./node_modules/.bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/nuthanc/bin:/usr/local/anaconda3/bin:/Users/nuthanc/.npm/bin:./node_modules/.bin:/Users/nuthanc/golang/bin:/usr/local/opt/go/libexec/bin:/Users/nuthanc/golang/bin:/usr/local/opt/go/libexec/bin
+export XPC_SERVICE_NAME=0
+export SECURITYSESSIONID=186a9
+export USER=nuthanc
+export XPC_FLAGS=0x0
+export GOPATH=/Users/nuthanc/golang
+export GOROOT=/usr/local/opt/go/libexec
+export LESS=-R
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+export NODE_PATH=/usr/local/lib/node_modules
+export OLDPWD=/Users/nuthanc/personal_projects/kali
+export PAGER=less
+export PWD=/Users/nuthanc/personal_projects/kali
+export SHLVL=1
+export ZSH=/Users/nuthanc/.oh-my-zsh
+export TERM_PROGRAM=vscode
+export TERM_PROGRAM_VERSION=1.44.0
+export LANG=en_GB.UTF-8
+export COLORTERM=truecolor
+export TERM=xterm-256color
+export _=/usr/bin/printenv
 
 git add --all
-git commit -m "Add git"
+git commit -m "Update git"
 git push origin master
